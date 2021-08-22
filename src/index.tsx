4c7d8b693bfc16a5dac808ea3c88c46dd88c1479
takurinton <takurinton@takurinton.com>
2021-08-22 19:28:07 +0900

    add: use react component

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOMServer from "react-dom/server";
 import express from 'express';
 
+const App = () => <h1>hello takurinton</h1>
+
 const app = express();
-app.get("/", (req: any, res: any) => {
-  res.send('<h1>hello world</h1>');
+app.get("/", (_, res: any) => {
+  res.send(ReactDOMServer.renderToString(<App />));
 });
 
 app.listen(3000, () => {
